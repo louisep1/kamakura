1543a11ee0a92415264c5dcc476ed60264e1e8af
louisep1 <89385533+louisep1@users.noreply.github.com>
2021-12-15 00:23:18 +0900

    mobile nav list click close

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+
+// TO DO: Make the menu close when you click outside of it
+
+
 const menuBtn = document.querySelector('.menu-btn');
 const mobileNav = document.querySelector('.nav-right');
 
-const destinationsBtn = document.querySelector('.destinations-dropdown');
+const destinationsBtn = document.querySelector('.destinations-dropdown a');
 const destinations = document.querySelectorAll('.destinations-dropdown-content li');
 
-const interestsBtn = document.querySelector('.interests-dropdown');
+const interestsBtn = document.querySelector('.interests-dropdown a');
 const interests = document.querySelectorAll('.interests-dropdown-content li');
 
-const hotBtn = document.querySelector('.hot-dropdown');
+const hotBtn = document.querySelector('.hot-dropdown a');
 const hot = document.querySelectorAll('.hot-dropdown-content li');
 
 
@@ -16,13 +20,13 @@ function openMenu(e) {
     mobileNav.classList.toggle('hidden');
     document.body.classList.toggle('no-scroll'); /* we created a no-scroll class in css to hide the hidden content in .hidden */
 
-    for(let i = 0; i< destinations.length; i++) {
+    for (let i = 0; i < destinations.length; i++) {
         destinations[i].style.display = 'none';
     }
-    for(let i = 0; i< interests.length; i++) {
+    for (let i = 0; i < interests.length; i++) {
         interests[i].style.display = 'none';
     }
-    for(let i = 0; i< hot.length; i++) {
+    for (let i = 0; i < hot.length; i++) {
         hot[i].style.display = 'none';
     }
 }
@@ -31,55 +35,73 @@ function openMenu(e) {
 
 // Opens the items and closes others
 function openDestinations(e) {
-    // for(let i = 0; i< destinations.length; i++) {
-    //     if (destinations[i].style.display == 'none') {
-    //         destinations[i].style.display = 'block';
-    //     } else {
-    //         destinations[i].style.display = 'none'
-    //     };
-    // }
-    // This worked, except for if you tried to click one of the sublinks, it also closes the section
-
-    for(let i = 0; i< destinations.length; i++) {
-        destinations[i].style.display = 'block';
+    for (let i = 0; i < destinations.length; i++) {
+        if (destinations[i].style.display == 'none') {
+            destinations[i].style.display = 'block';
+        } else {
+            destinations[i].style.display = 'none'
+        };
     }
+    // FIXED!!!! (This worked, except for if you tried to click one of the sublinks, it also closes the section)
+
+    // for (let i = 0; i < destinations.length; i++) {
+    //     destinations[i].style.display = 'block';
+    // }
 
 
-    for(let i = 0; i< interests.length; i++) {
+    for (let i = 0; i < interests.length; i++) {
         interests[i].style.display = 'none';
     }
-    for(let i = 0; i< hot.length; i++) {
+    for (let i = 0; i < hot.length; i++) {
         hot[i].style.display = 'none';
     }
 }
 
 function openInterests(e) {
-    for(let i = 0; i< interests.length; i++) {
-        interests[i].style.display = 'block';
+    for (let i = 0; i < interests.length; i++) {
+        if (interests[i].style.display == 'none') {
+            interests[i].style.display = 'block';
+        } else {
+            interests[i].style.display = 'none'
+        };
     }
-    
 
-    for(let i = 0; i< destinations.length; i++) {
+
+    // for (let i = 0; i < interests.length; i++) {
+    //     interests[i].style.display = 'block';
+    // }
+
+
+    for (let i = 0; i < destinations.length; i++) {
         destinations[i].style.display = 'none';
     }
-    for(let i = 0; i< hot.length; i++) {
+    for (let i = 0; i < hot.length; i++) {
         hot[i].style.display = 'none';
     }
 }
 
 function openHot(e) {
-    for(let i = 0; i< hot.length; i++) {
-        hot[i].style.display = 'block';
+    for (let i = 0; i < hot.length; i++) {
+        if (hot[i].style.display == 'none') {
+            hot[i].style.display = 'block';
+        } else {
+            hot[i].style.display = 'none'
+        };
     }
 
+    // for (let i = 0; i < hot.length; i++) {
+    //     hot[i].style.display = 'block';
+    // }
 
-    for(let i = 0; i< destinations.length; i++) {
+
+    for (let i = 0; i < destinations.length; i++) {
         destinations[i].style.display = 'none';
     }
-    for(let i = 0; i< interests.length; i++) {
+    for (let i = 0; i < interests.length; i++) {
         interests[i].style.display = 'none';
     }
 }
+
 
 
 
@@ -89,3 +111,5 @@ menuBtn.addEventListener('click', openMenu);
 destinationsBtn.addEventListener('click', openDestinations);
 interestsBtn.addEventListener('click', openInterests);
 hotBtn.addEventListener('click', openHot);
+
+// document.addEventListener('click', closeMenu);
